0aba69c36176d7b56a4ebdcbe7f84e2281c40320
Abdulaha Islam <abdulaha@brightvessel.com>
2022-09-01 22:24:54 +0600

    model login

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,11 @@
         jQuery(".load-more").click(function(){
             jQuery(".load-more-items").toggleClass("block");
         });
+        jQuery(".forget").click(function(){
+            jQuery(".login-form").toggleClass("d-none");
+            jQuery(".modal-footer").toggleClass("d-none");
+            jQuery(".forget-pass").toggleClass("d-block");
+        });
 
     });
 
