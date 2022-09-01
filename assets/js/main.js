208aae261fc8065cf3026fe81f8bd63a6cf9d645
Abdulaha Islam <abdulaha@brightvessel.com>
2022-09-01 21:26:49 +0600

    home page design done

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,10 @@
 
     jQuery(document).ready(function($) {
 
+        // search toogle class
+        jQuery(".load-more").click(function(){
+            jQuery(".load-more-items").toggleClass("block");
+        });
 
     });
 
