3aca42c3a982acfd4d7d53b6d6a4344cd383cf94
Abdulaha Islam <112110002+abdulaha-bv@users.noreply.github.com>
2022-09-06 00:44:29 +0600

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,17 @@
             jQuery(".login-login-fp").toggleClass("d-block");
         });
 
+        // scroll to top and menu fixed
+        $(window).scroll(function() {    
+            var scroll = $(window).scrollTop();
+
+            if ( scroll >= 95 ) {
+                $(".navbar").addClass("fixed");
+            } else {
+                $(".navbar").removeClass("fixed");
+            }
+        });
+
     });
 
 }(jQuery));
